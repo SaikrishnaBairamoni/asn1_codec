37133f3024b752ebb6f7d2d28906a10c4a445f90
Saikrishna Bairamoni <84093461+SaikrishnaBairamoni@users.noreply.github.com>
2023-08-03 15:53:40 -0400

    try

--- inspect_pr.py
+++ inspect_pr.py
@@ -1,41 +1,3 @@
-# import os
-# import requests
-
-# GITHUB_API_URL = "https://api.github.com"
-# BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Set your GitHub access token as an environment variable
-
-# def check_issue_reference(repo_full_name, pr_number):
-#     url = f"{GITHUB_API_URL}/repos/{repo_full_name}/pulls/{pr_number}/commits"
-#     headers = {"Authorization": f"token {BOT_TOKEN}"}
-#     response = requests.get(url, headers=headers)
-#     commits = response.json()
-
-#     for commit in commits:
-#         if "message" in commit["commit"] and "issue #" in commit["commit"]["message"].lower():
-#             return True
-
-#     return False
-
-# def add_comment_to_pr(repo_full_name, pr_number):
-#     url = f"{GITHUB_API_URL}/repos/{repo_full_name}/issues/{pr_number}/comments"
-#     headers = {"Authorization": f"token {BOT_TOKEN}"}
-#     data = {
-#         "body": "This pull request does not reference a GitHub issue. Please link to an issue in the PR description."
-#     }
-#     response = requests.post(url, headers=headers, json=data)
-#     if response.status_code == 201:
-#         print("Comment added successfully!")
-#     else:
-#         print(f"Failed to add comment. Status code: {response.status_code}")
-
-# if __name__ == "__main__":
-#     repo_full_name = os.environ.get("GITHUB_REPOSITORY")
-#     pr_number = os.environ.get("PR_NUMBER")
-
-#     has_issue_reference = check_issue_reference(repo_full_name, pr_number)
-
-#     if not has_issue_reference:
-#         add_comment_to_pr(repo_full_name, pr_number)
 import os
 import requests
 
@@ -54,10 +16,48 @@ def check_issue_reference(repo_full_name, pr_number):
 
     return False
 
+def add_comment_to_pr(repo_full_name, pr_number):
+    url = f"{GITHUB_API_URL}/repos/{repo_full_name}/issues/{pr_number}/comments"
+    headers = {"Authorization": f"token {BOT_TOKEN}"}
+    data = {
+        "body": "This pull request does not reference a GitHub issue. Please link to an issue in the PR description."
+    }
+    response = requests.post(url, headers=headers, json=data)
+    if response.status_code == 201:
+        print("Comment added successfully!")
+    else:
+        print(f"Failed to add comment. Status code: {response.status_code}")
+
 if __name__ == "__main__":
-    # You can add code to parse command-line arguments, receive input, etc.
-    repo_full_name = "your-username/your-repo"
-    pr_number = 1  # Replace with the actual PR number
+    repo_full_name = os.environ.get("GITHUB_REPOSITORY")
+    pr_number = os.environ.get("PR_NUMBER")
 
     has_issue_reference = check_issue_reference(repo_full_name, pr_number)
-    print(has_issue_reference)
+
+    if not has_issue_reference:
+        add_comment_to_pr(repo_full_name, pr_number)
+# import os
+# import requests
+
+# GITHUB_API_URL = "https://api.github.com"
+# BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Set your GitHub access token as an environment variable
+
+# def check_issue_reference(repo_full_name, pr_number):
+#     url = f"{GITHUB_API_URL}/repos/{repo_full_name}/pulls/{pr_number}/commits"
+#     headers = {"Authorization": f"token {BOT_TOKEN}"}
+#     response = requests.get(url, headers=headers)
+#     commits = response.json()
+
+#     for commit in commits:
+#         if "message" in commit["commit"] and "issue #" in commit["commit"]["message"].lower():
+#             return True
+
+#     return False
+
+# if __name__ == "__main__":
+#     # You can add code to parse command-line arguments, receive input, etc.
+#     repo_full_name = "your-username/your-repo"
+#     pr_number = 1  # Replace with the actual PR number
+
+#     has_issue_reference = check_issue_reference(repo_full_name, pr_number)
+#     print(has_issue_reference)
